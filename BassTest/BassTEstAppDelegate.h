f5840ef3f442a8981c3b151999580aa7b190f824
George Villasboas <george@logics.com.br>
2013-03-21 18:07:14 -0300

    Initial Commit

---
+++ BassTest/BassTEstAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BassTEstAppDelegate.h
+//  BassTest
+//
+//  Created by George Henrique Villasboas on 21/03/13.
+//  Copyright (c) 2013 George Henrique Villasboas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BassTEstAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BassTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BassTest
+//
+//  Created by George Henrique Villasboas on 21/03/13.
+//  Copyright (c) 2013 George Henrique Villasboas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BassTEstAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BassTEstAppDelegate class]));
+    }
+}
